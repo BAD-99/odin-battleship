b7d54c43d98bd07a494a5242f8e43db55e308737
BAD-99 <brandon.andrew.davis@gmail.com>
2024-05-05 00:44:13 -0600

    Added index and style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './style.css';
+import "./style.css";
+import * as dc from "./domController.js";
+import * as gc from "./gameController.js";
+import { displayPlayer } from "./domController.js";
+let player = 1;
+displayPlayer(0);
+
+// gc.placeRandom(gc.gameGetPlayers().player1);
+// dc.updateGameboard(dc.domGetPlayers().player1.board, gc.gameGetPlayers().player1);
+gc.playGame();
